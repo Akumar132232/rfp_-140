a5ab22e2a0639fe1f5c2a93c99e9430b0710869a
Akumar132232 <64557746+Akumar132232@users.noreply.github.com>
2022-05-12 02:17:42 +0530

    Add files via upload

---
+++ LinkedList/src/com/bridglabz/DataStructure.java
@@ -0,0 +1,15 @@
+package com.bridglabz;
+
+public class DataStructure {
+    public static void main(String[] args) {
+        LinkedList<Integer> LinkedList=new LinkedList();
+        LinkedList.push(56);
+        LinkedList.push(30);
+        LinkedList.push(70);
+        LinkedList.pop();
+        LinkedList.print();
+
+        LinkedList.Search(30);
+
+    }
+}
---
+++ LinkedList/src/com/bridglabz/LinkedList.java
@@ -0,0 +1,67 @@
+package com.bridglabz;
+
+public class LinkedList<T> {
+    Node<T> head;
+    Node<T> tail;
+
+    public void push(T data) {
+        Node<T> obj1 = new Node<>(data);
+        if (obj1 == null) {
+            head = obj1;
+            tail = obj1;
+        } else {
+            obj1.next = head;
+            head = obj1;
+        }
+    }
+
+    public void add(T data) {
+        Node<T> obj2 = new Node<>(data);
+        if (head == null) {
+            head = obj2;
+            tail = obj2;
+        } else {
+            tail.next = obj2;
+            tail = obj2;
+        }
+    }
+
+    public void insertBetween(T insertData) {
+        Node<T> obj2 = new Node<>(insertData);
+        head.next = obj2;
+        obj2.next = tail;
+    }
+
+    public void print() {
+        Node<T> temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+    }
+    public T pop() {
+        Node<T> popelement = head;
+        T element=popelement.data;
+        Node<T> nextelem=popelement.next;
+        popelement.data=null;
+        popelement.next=null;
+        head=nextelem;
+        return element;
+
+    }
+    public Node<T> Search(T searchData)
+    {
+        Node<T> temp=head;
+        Node<T> searchedNode=null;
+        while(temp!=null)
+        {
+            if(temp.data.equals(searchData))
+            {
+                searchedNode=temp;
+
+            }
+            temp=temp.next;
+        }
+        return searchedNode;
+    }
+}
